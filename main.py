2100b704ea158232847988b65d3d94899703c5d9
zawuoff <fouwaz232@gmail.com>
2026-05-03 01:55:15 +0100

    refactored the code a lil, added a new file to handle all the functions

--- main.py
+++ main.py
@@ -1,63 +1,19 @@
-import data
-
-def calculate_kda(kills, deaths, assists):
-    if deaths == 0:
-        return kills + assists
-    else:
-        return (kills + assists) / deaths
-
-def calculate_cs_per_min(cs, game_minutes):
-    if cs == 0:
-        return 0.0
-    else:
-        return cs/game_minutes
-
-def winrate(matches_list):
-    wins = 0
-    for match in matches_list:
-        if match["win"] == "True":
-            wins +=1
-    winrate = wins/len(matches_list) * 100
-    return round(winrate,1)
-
-def avg_cs(matches_list):
-    cs_per_minute_total = 0
-    for match in matches_list:
-        cs_per_minute_total += calculate_cs_per_min(match["cs"], match["game_minutes"])
-    cs_per_minute_avg = cs_per_minute_total/len(matches_list) 
-    return round(cs_per_minute_avg,1)
-
-def avg_kda(matches_list):
-    total_kda = 0
-    for match in matches_list:
-        total_kda += calculate_kda(match["kills"], match["deaths"], match["assists"])
-    average_kda = total_kda/ len(matches_list)
-    return average_kda
-
-def find_best_champ(matches_list):
-    best_champ = ""
-    best_kda = 0
-    for match in matches_list:
-        current_kda = calculate_kda(match["kills"], match["deaths"], match["assists"])
-        if current_kda > best_kda:
-            best_kda = current_kda
-            best_champ = match["champion"]
-    return best_champ
-
+import data, stats
 
 print(
+    "\n"
     f"RiftCoach Match Review\n"
     "========================\n"
     f"Games Analysed: {len(data.matches)}\n"
-    f"Win rate: {winrate(data.matches)}\n"
-    f"Average KDA: {avg_kda(data.matches)}\n"
-    f"Average CS per min: {avg_cs(data.matches)}\n"
-    f"Best Champion: {find_best_champ(data.matches)}"
+    f"Win rate: {stats.winrate(data.matches)}\n"
+    f"Average KDA: {stats.avg_kda(data.matches)}\n"
+    f"Average CS per min: {stats.avg_cs(data.matches)}\n"
+    f"Best Champion: {stats.find_best_champ(data.matches)}"
 )
 
-if avg_kda(data.matches) < 3:
+if stats.avg_kda(data.matches) < 3:
     print("Overall focus: play safer and reduce deaths")
-elif avg_cs(data.matches) < 6:
+elif stats.avg_cs(data.matches) < 6:
     print("Overall focus: improve farming")
 else:
     print("Overall focus: good overall performance")
@@ -65,8 +21,8 @@ else:
 print("=============================")
 
 for match in data.matches:
-    cs_per_minute = calculate_cs_per_min(match["cs"], match["game_minutes"])
-    kda = calculate_kda(match["kills"], match["deaths"], match["assists"])
+    cs_per_minute = stats.calculate_cs_per_min(match["cs"], match["game_minutes"])
+    kda = stats.calculate_kda(match["kills"], match["deaths"], match["assists"])
 
     print(
     f"Champion: {match['champion']}\n"
@@ -86,6 +42,3 @@ for match in data.matches:
         print("Try to last hit well!")
     
     print("---------------------------------------")
-
-
-print()
---
+++ stats.py
@@ -0,0 +1,43 @@
+def calculate_kda(kills, deaths, assists):
+    if deaths == 0:
+        return kills + assists
+    else:
+        return (kills + assists) / deaths
+
+def calculate_cs_per_min(cs, game_minutes):
+    if cs == 0:
+        return 0.0
+    else:
+        return cs/game_minutes
+
+def winrate(matches_list):
+    wins = 0
+    for match in matches_list:
+        if match["win"] == "True":
+            wins +=1
+    winrate = wins/len(matches_list) * 100
+    return round(winrate,1)
+
+def avg_cs(matches_list):
+    cs_per_minute_total = 0
+    for match in matches_list:
+        cs_per_minute_total += calculate_cs_per_min(match["cs"], match["game_minutes"])
+    cs_per_minute_avg = cs_per_minute_total/len(matches_list) 
+    return round(cs_per_minute_avg,1)
+
+def avg_kda(matches_list):
+    total_kda = 0
+    for match in matches_list:
+        total_kda += calculate_kda(match["kills"], match["deaths"], match["assists"])
+    average_kda = total_kda/ len(matches_list)
+    return average_kda
+
+def find_best_champ(matches_list):
+    best_champ = ""
+    best_kda = 0
+    for match in matches_list:
+        current_kda = calculate_kda(match["kills"], match["deaths"], match["assists"])
+        if current_kda > best_kda:
+            best_kda = current_kda
+            best_champ = match["champion"]
+    return best_champ
