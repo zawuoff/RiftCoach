866d5c256b4d38b3d4a7ec1f9703d39cad5bda01
zawuoff <fouwaz232@gmail.com>
2026-05-04 23:19:04 +0100

    moved everything to a main function and then called it at the end, also fixed some conditions

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 import data, stats
-
-print(
-    "\n"
-    f"RiftCoach Match Review\n"
-    "========================\n"
-    f"Games Analysed: {len(data.matches)}\n"
-    f"Win rate: {stats.winrate(data.matches)}\n"
-    f"Average KDA: {stats.avg_kda(data.matches)}\n"
-    f"Average CS per min: {stats.avg_cs(data.matches)}\n"
-    f"Average Vision Score: {stats.avg_vision_score(data.matches)}\n"
-    f"Best Champion: {stats.find_best_champ(data.matches)}"
-)
-
-if stats.avg_kda(data.matches) < 3:
-    print("Overall focus: play safer and reduce deaths")
-elif stats.avg_cs(data.matches) < 6:
-    print("Overall focus: improve farming")
-elif stats.avg_vision_score(data.matches) < 25:
-    print("Overall focus: improve warding")
-else:
-    print("Overall focus: good overall performance")
-
-print("=============================")
-
-for match in data.matches:
-    cs_per_minute = stats.calculate_cs_per_min(match["cs"], match["game_minutes"])
-    kda = stats.calculate_kda(match["kills"], match["deaths"], match["assists"])
+def main():
 
     print(
-    f"Champion: {match['champion']}\n"
-    f"KDA: {kda}\n"
-    f"CS/min: {cs_per_minute}\n"
-    "Feedback:"
+        "\n"
+        f"RiftCoach Match Review\n"
+        "========================\n"
+        f"Games Analysed: {len(data.matches)}\n"
+        f"Win rate: {stats.winrate(data.matches)}\n"
+        f"Average KDA: {stats.avg_kda(data.matches)}\n"
+        f"Average CS per min: {stats.avg_cs(data.matches)}\n"
+        f"Average Vision Score: {stats.avg_vision_score(data.matches)}\n"
+        f"Best Champion: {stats.find_best_champ(data.matches)}"
     )
 
-    if kda > 3:
-        print("Good fighting performance.")
-    else: 
-        print("Try to die less")
-    
-    if cs_per_minute > 7:
-        print("You are cs-ing pretty well this game")
+    if stats.avg_kda(data.matches) <= 3:
+        print("Overall focus: play safer and reduce deaths")
+    elif stats.avg_cs(data.matches) <= 6:
+        print("Overall focus: improve farming")
+    elif stats.avg_vision_score(data.matches) <= 25:
+        print("Overall focus: improve warding")
     else:
-        print("Try to last hit well!")
-    
-    print("---------------------------------------")
+        print("Overall focus: good overall performance")
+
+    print("=============================")
+
+    for match in data.matches:
+        cs_per_minute = stats.calculate_cs_per_min(match["cs"], match["game_minutes"])
+        kda = stats.calculate_kda(match["kills"], match["deaths"], match["assists"])
+
+        print(
+        f"Champion: {match['champion']}\n"
+        f"KDA: {kda}\n"
+        f"CS/min: {cs_per_minute}\n"
+        "Feedback:"
+        )
+
+        if kda >= 3:
+            print("Good fighting performance.")
+        else: 
+            print("Try to die less")
+        
+        if cs_per_minute >= 7:
+            print("You are cs-ing pretty well this game")
+        else:
+            print("Try to last hit well!")
+        
+        print("---------------------------------------")
+
+main()
