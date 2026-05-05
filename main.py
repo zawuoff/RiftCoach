39d75d11ffc8a3170974001b85c591833dad9811
zawuoff <fouwaz232@gmail.com>
2026-05-06 00:16:39 +0100

    few more refactoring to make it cleaner so fucntions have purpose, rather than one fucntion doing everything

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def print_overall_summary():
         f"Best Champion: {stats.find_best_champ(data.matches)}"
     )
 
-def print_individual_summary():
     if stats.avg_kda(data.matches) <= 3:
         print("Overall focus: play safer and reduce deaths")
     elif stats.avg_cs(data.matches) <= 6:
@@ -25,32 +24,35 @@ def print_individual_summary():
 
     print("=============================")
 
-    for match in data.matches:
-        cs_per_minute = stats.calculate_cs_per_min(match["cs"], match["game_minutes"])
-        kda = stats.calculate_kda(match["kills"], match["deaths"], match["assists"])
+
+def print_match_review(matches):
+        cs_per_minute = stats.calculate_cs_per_min(matches["cs"], matches["game_minutes"])
+        kda = stats.calculate_kda(matches["kills"], matches["deaths"], matches["assists"])
 
         print(
-        f"Champion: {match['champion']}\n"
+        f"Champion: {matches['champion']}\n"
         f"KDA: {kda}\n"
         f"CS/min: {cs_per_minute}\n"
         "Feedback:"
         )
-
         if kda >= 3:
             print("Good fighting performance.")
         else: 
             print("Try to die less")
-        
         if cs_per_minute >= 7:
             print("You are cs-ing pretty well this game")
         else:
             print("Try to last hit well!")
-        
         print("---------------------------------------")
+    
+
+def print_individual_summary(matches):
+    for match in matches:
+        print_match_review(match)
 
 def main():
     print_overall_summary()
-    print_individual_summary()
+    print_individual_summary(data.matches)
     
 
 main()
